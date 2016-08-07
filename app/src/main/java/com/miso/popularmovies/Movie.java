f12c78d39ac0e12513b105731b014ed637506af2
M187 <dony66@gmail.com>
2016-08-07 22:20:15 +0200

    Initial commit

---
+++ app/src/main/java/com/miso/popularmovies/Movie.java
@@ -0,0 +1,16 @@
+package com.miso.popularmovies;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Miso on 7.8.2016.
+ */
+public class Movie {
+
+    String pictureUrl;
+    String id;
+
+    public Movie(String pictureUrl){
+        this.pictureUrl = pictureUrl;
+    }
+}
